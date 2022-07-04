af02650bb9b760ee2113c093bb68a93de96cd90e
Tomas Benitez Vescio <tbvescio@gmail.com>
2022-07-04 09:08:51 -0300

    add swagger template

--- src/__init__.py
+++ src/__init__.py
@@ -2,8 +2,9 @@
 from flask import Flask
 from src.users import users
 from src.tasks import tasks
-from flasgger import Swagger
+from flasgger import Swagger, LazyString
 from flask_cors import CORS
+
 
 def create_app():
     app = Flask(__name__)
@@ -13,6 +14,16 @@ def create_app():
     app.register_blueprint(users)
     app.register_blueprint(tasks)
 
-    Swagger(app)
+    template = {
+        "info": {
+            "title": "Wazuh Test API",
+            "version": "0.0.1"
+        },
+        "schemes": [
+            "http",
+            "https"
+        ],
+    }
+    Swagger(app, template=template)
 
     return app
